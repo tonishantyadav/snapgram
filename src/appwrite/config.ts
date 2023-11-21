dcb43046a59d2a49251b7e4c5685b7a43f194ecb
Nishant Yadav <inishantyadav24@gmail.com>
2023-11-21 07:53:10 +0530

    Config: appwrite related settings

--- src/appwrite/config.ts
+++ src/appwrite/config.ts
@@ -1,8 +1,13 @@
 import { Account, Avatars, Client, Databases, Storage } from "appwrite";
 
 export const appwriteConfig = {
-  projectId: import.meta.env.VITE_APPWRITE_PROJECT_ID,
   url: import.meta.env.VITE_APPWRITE_URL,
+  projectId: import.meta.env.VITE_APPWRITE_PROJECT_ID,
+  storageId: import.meta.env.VITE_APPWRITE_STORAGE_ID,
+  databaseId: import.meta.env.VITE_APPWRITE_DATABASE_ID,
+  userCollectionId: import.meta.env.VITE_APPWRITE_USER_COLLECTION_ID,
+  postCollectionId: import.meta.env.VITE_APPWRITE_POST_COLLECTION_ID,
+  saveCollectionId: import.meta.env.VITE_APPWRITE_SAVE_COLLECTION_ID,
 };
 
 export const client = new Client();
@@ -10,6 +15,6 @@ client.setProject(appwriteConfig.projectId);
 client.setEndpoint(appwriteConfig.url);
 
 export const account = new Account(client);
-export const db = new Databases(client);
+export const database = new Databases(client);
 export const storage = new Storage(client);
 export const avatars = new Avatars(client);
